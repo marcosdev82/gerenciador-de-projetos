7c8efd0e38ee077ab014cbac9aea1f31d62f6791
mikek182 <marcosvinicius@geekwork.com.br>
2024-11-30 16:27:56 -0300

    Criação de rotas para listar tarefas

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const cors = require('cors'); // Libera acessos de domínios diferentes
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
-app.listen(port, () = console.log(`Servidor inicializado na porta ${port}`) )
+// Configuração do middleware
+app.use(cors({ credentials: true, origin: '*' }));
+app.use(bodyParser.json());
+
+function naoImplementado(req, res) {
+    res.status(501).json({ erro: 'Não implementado' });
+}
+
+// Rota para listar todas as tarefas
+app.get('/gerenciador-tarefas', (req, res) => naoImplementado);
+// Rota para listar tarefas por ID
+app.get('/gerenciador-tarefas/:id', (req, res) => naoImplementado);
+
+// Inicialização do servidor
+app.listen(port, () => {
+  console.log(`Servidor inicializado na porta ${port}`);
+}).on('error', (err) => {
+  console.error(`Erro ao iniciar o servidor: ${err.message}`);
+});
