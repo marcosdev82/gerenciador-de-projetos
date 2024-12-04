1090b71c88518a65f2668e9cae62545cbe77e8a7
mikek182 <marcosvinicius@geekwork.com.br>
2024-12-04 13:20:53 -0300

    Retorno do autalizar tarefa

--- controllers/gerenciador-tarefas.js
+++ controllers/gerenciador-tarefas.js
@@ -71,14 +71,26 @@ function atualizarTarefa(req, res) {
     }
 
     const id = req.params.id
+    let tarefaAtualizada = false
 
     tarefas = tarefas.map(tarefa => {
         if (tarefa.id === id) {
             tarefa.nome = req.body['nome']
             tarefa.concluida = req.body['concluida']
+            tarefaAtualizada = true
         }
         return tarefa;
     });
+    
+    if (!tarefaAtualizada) {
+        res.status(404).json({erro: 'Tarefa não encontrada.'});
+    }
+
+    res.json({
+        id: id,
+        nome: req.body['nome'],
+        consluida: req.body['concluida']
+    })
 
 }
 
